c5d616aba82e4814392859e9f576f896c901e634
idaliaFelsmann <105213955+idaliaFelsmann@users.noreply.github.com>
2022-10-03 20:13:03 +0200

    structure, bootstrap and logo added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.png";
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+    
+      <header>
+       <div className="logo-dict" >
+      <img src={logo} alt="logo" className="img-fluid"></img>
+      </div>
+      </header>
+    </div>
+  );
+}
+
+
